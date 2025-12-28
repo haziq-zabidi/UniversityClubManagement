02fcd5fdb0e5c6f7247f297ea67723f54deedc90
haziq-zabidi <haziqzabidi2004@gmail.com>
2025-12-29 07:35:19 +0800

    update register sikit

--- src/main/java/com/mycompany/controller/RegisterController.java
+++ src/main/java/com/mycompany/controller/RegisterController.java
@@ -4,10 +4,28 @@
  */
 package com.mycompany.controller;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  *
  * @author TUF
  */
-public class RegisterController {
+@WebServlet("/register")
+public class RegisterController extends HttpServlet{
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/views/register.jsp"); 
+        dispatcher.forward(request, response);
+    }
     
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
 }
